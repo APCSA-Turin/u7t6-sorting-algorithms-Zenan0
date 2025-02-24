bd2c5df4b03f0bf0995f8bc66fc0af3bed7653a1
Zenan0 <zenanl@nycstudents.net>
2025-02-24 17:01:23 +0000

    message

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -8,14 +8,35 @@ public class SelectionSort {
 
     // PART A. implementing selection sort
     public static int[] selectionSort(int[] elements) {
-       
+       for (int i = 0; i < elements.length - 1; i++) {
+        int min = elements[i];
+        int minIdx = i;
+        for (int j = i + 1; j < elements.length; j++) {
+            if (elements[j] < min) {
+                min = elements[j];
+                minIdx = j;
+            }
+        }
+        elements[minIdx] = elements[i];
+        elements[i] = min;
+       }
         return elements;
     }
 
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
-       
+       for (int i = 0; i < words.size() - 1; i++) {
+        String firstWord = words.get(i);
+        int firstWordIdx = i;
+        for (int j = i + 1; j < words.size(); j++) {
+            if (words.get(j).compareTo(firstWord) < 0) {
+                firstWord = words.get(j);
+                firstWordIdx = j;
+            }
+        }
+        words.set(firstWordIdx, words.set(i, firstWord));
+       }
         return words;
     }
 
